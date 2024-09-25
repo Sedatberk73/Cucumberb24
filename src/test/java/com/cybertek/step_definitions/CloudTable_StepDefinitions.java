10075675acdffee64ff887ace195968366da874a
sedat_73 <sdtbrk93@gmail.com>
2024-09-25 00:22:25 -0600

    send remaining data

--- src/test/java/com/cybertek/step_definitions/CloudTable_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/CloudTable_StepDefinitions.java
@@ -1,74 +1,74 @@
-//package com.cybertek.step_definitions;
-//
-//import com.cybertek.pages.CloudTablePages;
-//import com.cybertek.utilities.BrowserUtils;
-//import com.cybertek.utilities.ConfigurationReader;
-//import com.cybertek.utilities.Driver;
-//import io.cucumber.java.Before;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.When;
-//import org.junit.Assert;
-//import org.openqa.selenium.WebDriver;
-//
-//public class CloudTable_StepDefinitions {
-//    WebDriver driver;
-//    CloudTablePages tablePages=new CloudTablePages();
-//
-//    @Before
-//    public void setUp(){
-//        driver= Driver.getDriver();
-//    }
-//
-//
-//
-//    @Given("User is on cloudtables homepage")
-//    public void user_is_on_cloudtables_homepage() {
-//        driver.get(ConfigurationReader.getProperty("cloudTable.url"));
-//
-//
-//    }
-//
-//    @When("User  clicks on New button")
-//    public void userClicksOnNewButton() {
-//        tablePages.NewButton.click();
-//        Assert.assertEquals("Editor | Editing for DataTables",driver.getTitle());
-//    }
-//
-//
-//
-//    @When("User enters {string} to firstname field")
-//    public void user_enters_to_firstname_field(String firstname) {
-//        tablePages.firstName.sendKeys(firstname);
-//        BrowserUtils.sleep(2);
-//
-//
-//    }
-//    @When("User enter {string} to lastname field")
-//    public void user_enter_to_lastname_field(String lastname) {
-//        tablePages.lastName.sendKeys(lastname);
-//
-//
-//    }
-//    @When("User enter {string} to position field")
-//    public void user_enter_to_position_field(String position) {
-//
-//        tablePages.positions.sendKeys(position);
-//
-//    }
-//    @When("User enter {string} to salary field")
-//    public void user_enter_to_salary_field(String salary) {
-//        tablePages.salary.sendKeys(salary);
-//        BrowserUtils.sleep(2);
-//
-//
-//    }
-//    @When("User clicks on create button")
-//    public void user_clicks_on_create_button() {
-//        tablePages.createButton.click();
-//
-//
-//    }
-//
-//
-//
-//}
+package com.cybertek.step_definitions;
+
+import com.cybertek.pages.CloudTablePages;
+import com.cybertek.utilities.BrowserUtils;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class CloudTable_StepDefinitions {
+    WebDriver driver;
+    CloudTablePages tablePages=new CloudTablePages();
+
+    @Before
+    public void setUp(){
+        driver= Driver.getDriver();
+    }
+
+
+
+    @Given("User is on cloudtables homepage")
+    public void user_is_on_cloudtables_homepage() {
+        driver.get(ConfigurationReader.getProperty("cloudTable.url"));
+
+
+    }
+
+    @When("User  clicks on New button")
+    public void userClicksOnNewButton() {
+        tablePages.NewButton.click();
+        Assert.assertEquals("Editor | Editing for DataTables",driver.getTitle());
+    }
+
+
+
+    @When("User enters {string} to firstname field")
+    public void user_enters_to_firstname_field(String firstname) {
+        tablePages.firstName.sendKeys(firstname);
+        BrowserUtils.sleep(2);
+
+
+    }
+    @When("User enter {string} to lastname field")
+    public void user_enter_to_lastname_field(String lastname) {
+        tablePages.lastName.sendKeys(lastname);
+
+
+    }
+    @When("User enter {string} to position field")
+    public void user_enter_to_position_field(String position) {
+
+        tablePages.positions.sendKeys(position);
+
+    }
+    @When("User enter {string} to salary field")
+    public void user_enter_to_salary_field(String salary) {
+        tablePages.salary.sendKeys(salary);
+        BrowserUtils.sleep(2);
+
+
+    }
+    @When("User clicks on create button")
+    public void user_clicks_on_create_button() {
+        tablePages.createButton.click();
+
+
+    }
+
+
+
+}
--- src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
@@ -8,7 +8,11 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
 
+import java.util.List;
+
 public class Google_StepDefinitions {
+
+    GoogleSearchPage searchPage = new GoogleSearchPage();
 
     @Given("User is on Google home page")
     public void user_is_on_google_home_page() {
@@ -34,5 +38,29 @@ public class Google_StepDefinitions {
 
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
+    }
+
+    @Then("User should be able to search for following:")
+    public void userShouldBeAbleToSearchForFollowing(List<String> items) {
+        System.out.println("items = " + items);
+        //loop and search for each item on google
+        GoogleSearchPage searchPage = new GoogleSearchPage();
+
+        for(String eachItem : items) {
+            searchPage.searchBar.clear();
+            searchPage.searchBar.sendKeys(eachItem + Keys.ENTER);
+        }
+    }
+
+    @When("User searches for {string} capital")
+    public void userSearchesForCapital(String countryName) {
+        System.out.println("Searching for capital city of " + countryName);
+        searchPage.searchBar.sendKeys("what is capital city of " + countryName + Keys.ENTER);
+    }
+
+    @Then("User should see {string} in the result")
+    public void userShouldSeeInTheResult(String capitalCity) {
+        System.out.println("Expected Capital City name: " + capitalCity);
+        Assert.assertEquals(capitalCity, searchPage.searchResultCityName.getText());
     }
 }
